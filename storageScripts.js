cdcf02a960973dbf91fb37a040efd3a56d180b8f
Dave Regg <drregg6@gmail.com>
2019-08-01 15:53:25 -0400

    Started storageScripts.js

---
+++ storageScripts.js
@@ -0,0 +1,33 @@
+const myStorage = window.localStorage;
+function storageAvailable(type) {
+  var storage;
+  try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+  }
+}
+if (storageAvailable('localStorage')) {
+  console.log(myStorage);
+}
+
+// storage is always key/value STRINGS
+if (!myStorage.getItem('myLibrary')) {
+  console.log('No library found');
+}
